325aa5d16150375ad49085a8f0d170c63c15de44
Astrotalk <astrotalk@Astrotalks-MacBook-Pro.local>
2025-09-12 21:36:03 +0530

    api endpoint updated

--- src/main/java/com/springbootBackend/backend/constants/ApiConstants.java
+++ src/main/java/com/springbootBackend/backend/constants/ApiConstants.java
@@ -6,7 +6,7 @@ public class ApiConstants {
     public static final String AUTH_BASE = API_BASE + "/auth";
 
 
-    public static final String EMAIL_SIGNUP ="/user/signup/email";
-    public static final String MOBILE_SIGNUP ="/user/signup/mobile";
+    public static final String EMAIL_SIGNUP ="/user/signup/email/get-otp";
+    public static final String MOBILE_SIGNUP ="/user/signup/mobile/get-otp";
 
 }
