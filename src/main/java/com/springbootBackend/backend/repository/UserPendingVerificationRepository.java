d52290516e7d3a478cd2c3645623abe46f14efca
Astrotalk <astrotalk@Astrotalks-MacBook-Pro.local>
2025-09-14 00:06:02 +0530

    minor chnage

--- src/main/java/com/springbootBackend/backend/repository/UserPendingVerificationRepository.java
+++ src/main/java/com/springbootBackend/backend/repository/UserPendingVerificationRepository.java
@@ -1,12 +1,17 @@
 package com.springbootBackend.backend.repository;
 
 import com.springbootBackend.backend.entity.UserPendingVerification;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserPendingVerificationRepository extends JpaRepository<UserPendingVerification,Long> {
     Optional<UserPendingVerification> findByPhoneNumber(String phoneNumber);
     Optional<UserPendingVerification> findByUserName(String userName);
+
+    @Transactional
+    int deleteAllByOtpExpiryTimeBefore(LocalDateTime now);
 
 }
