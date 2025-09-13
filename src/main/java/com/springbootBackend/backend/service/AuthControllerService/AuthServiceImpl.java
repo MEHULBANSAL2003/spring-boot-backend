a0d3b5103c995a2f8706aa2ed535ece04663d010
Astrotalk <astrotalk@Astrotalks-MacBook-Pro.local>
2025-09-13 15:42:32 +0530

    resend otp basic impl done

--- src/main/java/com/springbootBackend/backend/service/AuthControllerService/AuthServiceImpl.java
+++ src/main/java/com/springbootBackend/backend/service/AuthControllerService/AuthServiceImpl.java
@@ -2,12 +2,14 @@ package com.springbootBackend.backend.service.AuthControllerService;
 
 
 import com.springbootBackend.backend.dto.userMobileSignUpDto.MobileSignUpResponseDto;
+import com.springbootBackend.backend.dto.userMobileSignUpVerificationDto.UserMobileSignupVerificationResponseDto;
 import com.springbootBackend.backend.entity.UserPendingVerification;
 import com.springbootBackend.backend.exceptions.customExceptions.PhoneNumberAlreadyExistsException;
 import com.springbootBackend.backend.exceptions.customExceptions.UserNameExistsException;
 import com.springbootBackend.backend.helper.OtpGenerator;
 import com.springbootBackend.backend.repository.UserDataRepository;
 import com.springbootBackend.backend.repository.UserPendingVerificationRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,7 @@ public class AuthServiceImpl implements AuthService {
 
 
     @Override
+    @Transactional
     public MobileSignUpResponseDto mobileSignupGetOtp(String phoneNumber, String userName, String password) {
 
         if(userDataRepository.findByPhoneNumber(phoneNumber).isPresent()){
@@ -35,19 +38,35 @@ public class AuthServiceImpl implements AuthService {
 
         int otp = new OtpGenerator().generateOtp();
 
-        UserPendingVerification pendingUser = new UserPendingVerification();
-        pendingUser.setPhoneNumber(phoneNumber);
-        pendingUser.setCountryCode("+91");
-        pendingUser.setPassword(password);
-        pendingUser.setUserName(userName);
+        UserPendingVerification pendingUser = userPendingVerificationRepository
+                .findByUserName(userName)
+                .orElse(null);
+
+        if(pendingUser == null){
+            pendingUser = new UserPendingVerification();
+            pendingUser.setPhoneNumber(phoneNumber);
+           pendingUser.setCountryCode("+91");
+           pendingUser.setPassword(password);
+            pendingUser.setUserName(userName);
+            pendingUser.setOtp(String.valueOf(otp));
+            pendingUser.setIncorrectAttempts(0);
+            pendingUser.setOtpExpiryTime(LocalDateTime.now().plusMinutes(5));
+            pendingUser.setIsTwilioActive(true);
+
+        }
         pendingUser.setOtp(String.valueOf(otp));
-        pendingUser.setIncorrectAttempts(0);
         pendingUser.setOtpExpiryTime(LocalDateTime.now().plusMinutes(5));
-        pendingUser.setIsTwilioActive(true);
 
         userPendingVerificationRepository.save(pendingUser);
 
         MobileSignUpResponseDto response = new MobileSignUpResponseDto("success", 6, true,"Otp sent successfully", 300,true);
         return response;
+    }
+
+
+    @Override
+    @Transactional
+    public UserMobileSignupVerificationResponseDto mobileSignUpVerifyOtp(String userName, String otp) {
+        return null;
     }
 }
