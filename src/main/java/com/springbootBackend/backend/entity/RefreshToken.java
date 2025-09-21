b458bbdd963271eba411e93e9269a47d54c8e8e3
Astrotalk <astrotalk@Astrotalks-MacBook-Pro.local>
2025-09-21 22:20:02 +0530

    refrsh token entity setup

---
+++ src/main/java/com/springbootBackend/backend/entity/RefreshToken.java
@@ -0,0 +1,49 @@
+package com.springbootBackend.backend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserDataEntity user;
+
+    @Column(nullable = false, unique = true, length = 500)
+    private String refresh_token;
+
+    // Metadata
+    @Column(name = "device_info")
+    private String deviceInfo;
+
+    @Column(name = "ip_address", length = 45)
+    private String ipAddress;
+
+    @Column(name = "user_agent", length = 1000)
+    private String userAgent;
+
+    // Timestamps
+    @Column(nullable = false)
+    private Instant issuedAt;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    private boolean revoked = false;
+}
